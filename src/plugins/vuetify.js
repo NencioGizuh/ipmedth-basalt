67d787781867398fdcf0e5c6207178deb56789bf
Lara van Diemen <laravandiemen23@gmail.com>
2020-11-18 13:45:46 +0100

    Added Vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#0086d7",
+                secondary: "#cfecf7",
+                accent: "#ef7d00"
+            }
+        }
+    }
+});
