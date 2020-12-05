d2ed5d61a475141e4455f2efcbf0e495accb5672
echo-hakan <hakanf_@hotmail.com>
2020-12-05 13:03:48 +0100

    dynamic afbeeldingen toegevoegd + bug fixes

--- src/views/Medication/MedicationHelp.vue
+++ src/views/Medication/MedicationHelp.vue
@@ -1,12 +1,12 @@
 <template>
   <v-card elevation="24" max-width="444" class="mx-auto">
-    <v-carousel :continuous="false" :cycle="cycle" :show-arrows="true"  delimiter-icon="mdi-numeric-1" >
+    <v-carousel :continuous="false" :cycle="cycle" :show-arrows="true"  delimiter-icon="mdi-numeric-0" >
       <h3 align="center" class="medicijn_name" font-weight="500">{{ this.medicijnen[medicijnen.length - 1].title }}</h3>
       <v-carousel-item v-for="(slide, i) in slides" :key="i">
         <v-sheet color="blue-grey lighten-5" height="100%" >
           <v-row class="fill-height" align="center" justify="center">
             <div class="display-3">
-              <v-img src="@/assets/medicatie/medicatie_luchtverwijderaar_1.png"></v-img>
+              <img :src="images[i]" />
             </div>
             <div class="display_55">
               <h6> {{ slide }} </h6>
@@ -20,6 +20,13 @@
 </template>
 
 <script>
+
+import image1 from "@/assets/medicatie/medicatie_luchtverwijderaar_1.png"
+import image2 from "@/assets/medicatie/medicatie_luchtverwijderaar_2.png"
+import image3 from "@/assets/medicatie/medicatie_luchtverwijderaar_3.png"
+import image4 from "@/assets/medicatie/medicatie_luchtverwijderaar_4.png"
+import image5 from "@/assets/medicatie/medicatie_luchtverwijderaar_5.png"
+
 export default {
   data() {
     return {
@@ -37,15 +44,24 @@ export default {
         "Duw de hefboom weer omlaag en zet de beschermdop terug.",
         ],
         images: [
-          "@/assets/medicatie/medicatie_luchtverwijderaar_1.png",
+          image1,
+          image1,
+          image2,
+          image3,
+          image4,
+          image5,
+          image5,
+          image5,
+          image5,
         ]
     };
   },
   methods:{
     changeIcon(){ 
-      var ind = 0;
+      var ind;
       for (ind = 0; ind < this.slides.length; ind++) {
-        document.getElementsByClassName("v-icon notranslate mdi mdi-numeric-1 theme--dark")[0].className = "v-icon notranslate mdi mdi-numeric-"+ind +" theme--dark";
+        var elem_icon = parseInt(document.getElementsByClassName("v-icon notranslate mdi mdi-numeric-0 theme--dark")[0].parentElement.parentElement.value);
+        document.getElementsByClassName("v-icon notranslate mdi mdi-numeric-0 theme--dark")[0].className = "v-icon notranslate mdi mdi-numeric-"+(elem_icon+1) +" theme--dark";
       }
     }
   },
@@ -54,12 +70,10 @@ export default {
         return this.$store.getters.medicijnen_tijden;
     },
   },
-    mounted() {
-    document.onreadystatechange = () => {    
+    mounted() { 
       if (document.readyState == "complete") {  
           this.changeIcon();
       } 
-    }
   },
   beforeRouteEnter (to, from, next) {
     next(vm => {
