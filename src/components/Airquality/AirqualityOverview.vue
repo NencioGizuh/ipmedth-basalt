11c17cf72af4a06992f3d2400d96707c94dc7187
Nencio Paniagua <nencio.paniagua@pxlwidgets.com>
2020-12-05 14:00:47 +0100

    Rewritten logic for geolocation.

--- src/components/Airquality/AirqualityOverview.vue
+++ src/components/Airquality/AirqualityOverview.vue
@@ -1,17 +1,41 @@
 <template>
   <div>
-    <v-card v-if="initalized" class="mb-2" @click="overviewConcentration" elevation="2">
-        <v-list-item three-line>
-            <v-list-item-content>
-                <v-card-title class="overline caption mt-n3 mb-n9">{{ location }}<v-icon color="black" class="ml-1">mdi-crosshairs-gps</v-icon></v-card-title>
-                <v-card-title class="headline font-weight-bold">{{ currentAirquality }}</v-card-title>
-                <v-card-subtitle>{{ currentDate }}, lokale tijd</v-card-subtitle>
-            </v-list-item-content>
-        <v-progress-circular :color="colorStroke" :value="100" :size="75">{{ currentConcentration }}</v-progress-circular>
-        </v-list-item>
-      <bar :styles="myStyles" v-if="loaded" :chartdata="chartdata" :options="options"></bar>
+    <v-card
+      v-if="initalized"
+      class="mb-2"
+      @click="overviewConcentration"
+      elevation="2"
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-card-title class="overline caption mt-n3 mb-n9"
+            >{{ location
+            }}<v-icon color="black" class="ml-1"
+              >mdi-crosshairs-gps</v-icon
+            ></v-card-title
+          >
+          <v-card-title class="headline font-weight-bold">{{
+            currentAirquality
+          }}</v-card-title>
+          <v-card-subtitle>{{ currentDate }}, lokale tijd</v-card-subtitle>
+        </v-list-item-content>
+        <v-progress-circular :color="colorStroke" :value="100" :size="75">{{
+          currentConcentration
+        }}</v-progress-circular>
+      </v-list-item>
+      <bar
+        :styles="myStyles"
+        v-if="loaded"
+        :chartdata="chartdata"
+        :options="options"
+      ></bar>
     </v-card>
-    <v-skeleton-loader v-else width="382" height="221" type="list-item-three-line, card-avatar"></v-skeleton-loader>
+    <v-skeleton-loader
+      v-else
+      width="382"
+      height="221"
+      type="list-item-three-line, card-avatar"
+    ></v-skeleton-loader>
   </div>
 </template>
 
@@ -22,18 +46,19 @@ const { Geolocation } = Plugins;
 import axios from "axios";
 import AirqualityBar from "./AirqualityBar.vue";
 export default {
-  name: "Dashboard",
   data() {
     return {
       initalized: false,
       loaded: false,
       chartdata: {
         labels: [],
-        datasets: [{
+        datasets: [
+          {
             minBarLength: 2,
             backgroundColor: [],
-            data: []
-        }],
+            data: [],
+          },
+        ],
       },
       options: {
         responsive: true,
@@ -52,9 +77,9 @@ export default {
           yAxes: [
             {
               ticks: {
-                beginAtZero: true
+                beginAtZero: true,
               },
-              display: false, //this will remove all the x-axis grid lines 
+              display: false, //this will remove all the x-axis grid lines
             },
           ],
           xAxes: [
@@ -68,28 +93,172 @@ export default {
               time: {
                 unit: "minute",
                 displayFormats: {
-                    'minute': 'HH:mm',
+                  minute: "HH:mm",
                 },
               },
-              ticks: { 
-                  source: 'data',
-              }
+              ticks: {
+                source: "data",
+              },
             },
           ],
         },
       },
       color: "",
       airQualityPerHour: "",
-      height: 100
+      height: 100,
     };
   },
   components: {
-    'bar': AirqualityBar,
+    bar: AirqualityBar,
   },
   methods: {
+    initalizeComponent() {
+        this.checkDevice();
+    },
+    checkDevice() {
+        var vm = this;
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            this.getGeolocationMobile()
+            .then(function (response) {
+                console.log(response);
+                vm.reverseGeocoding();
+                vm.airConcentrations();
+            })
+            .catch(function (error) {
+                console.error(error);
+            })
+        }
+        else {
+            this.getGeolocationDesktop()
+            .then(function (response) {
+                console.log(response);
+                vm.reverseGeocoding();
+                vm.airConcentrations();
+            })
+            .catch(function (error) {
+                console.error(error);
+            })
+        }
+    },
+    getGeolocationMobile() {
+        return new Promise((resolve, reject) => {
+            Geolocation.getCurrentPosition()
+                .then(function (response) {
+                resolve(response);
+                console.log(response);
+                this.$store.state.longitude = response.coords.longitude;
+                this.$store.state.latitude = response.coords.latitude;
+            })
+            .catch(function (error) {
+                reject(error);
+                console.log(error);
+            });
+        })
+    },
+    getGeolocationDesktop() {
+        return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            console.log(position);
+            resolve(position);
+            this.$store.state.latitude = position.coords.latitude;
+            this.$store.state.longitude = position.coords.longitude;
+          },
+          (error) => {
+            reject(error);
+            switch (error.code) {
+              case error.PERMISSION_DENIED:
+                 console.error("User denied the request for Geolocation.");
+                break;
+              case error.POSITION_UNAVAILABLE:
+                console.error("Location information is unavailable.");
+                break;
+              case error.TIMEOUT:
+                console.error("The request to get user location timed out.");
+                break;
+              case error.UNKNOWN_ERROR:
+                console.error("An unknown error occurred.");
+                break;
+            }
+          }
+        );
+        });
+     },
+    reverseGeocoding() {
+        var vm = this;
+          axios.get("http://www.mapquestapi.com/geocoding/v1/reverse", {
+          params: {
+            key: "SGTAMDyeoX9iqBrAGn8KVEuKHAIvC2HK",
+            location:
+              this.$store.state.latitude + "," + this.$store.state.longitude,
+          },
+        })
+        .then(function (response) {
+          console.log(response);
+          console.log(response.data.results);
+          for (let index = 0; index < response.data.results.length; index++) {
+            const element = response.data.results[index];
+            for (let index = 0; index < element.locations.length; index++) {
+              const location = element.locations[index];
+              console.log(location);
+              vm.$store.state.location = location.street;
+              vm.$store.state.fullLocation =
+                location.street + ", " + location.adminArea5;
+              console.log(vm.$store.state.location);
+            }
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+    airConcentrations() {
+        var vm = this;
+        axios.get("https://api.luchtmeetnet.nl/open_api/concentrations", {
+          params: {
+            formula: "LKI",
+            longitude: this.$store.state.longitude,
+            latitude: this.$store.state.latitude,
+          },
+        })
+        .then(function (response) {
+          console.log(response.data.data);
+          for (let index = 0; index < response.data.data.length; index++) {
+            const element = response.data.data[index];
+            const currentTime = moment().format("YYYY-MMM-DD HH:00");
+            if (
+              moment(currentTime).isSame(
+                moment(element.timestamp_measured).format("YYYY-MM-DD HH:mm")
+              )
+            ) {
+              vm.colorScheme(element.value);
+              vm.$store.state.colorStroke = vm.color;
+              vm.$store.state.currentAirquality = vm.airQualityPerHour;
+              vm.$store.state.currentConcentration = Math.round(element.value);
+              vm.chartdata.labels.push(
+                moment(element.timestamp_measured).format("YYYY-MM-DD HH:mm")
+              );
+              vm.chartdata.datasets[0].backgroundColor.push(vm.color);
+              vm.chartdata.datasets[0].data.push(element.value);
+            } else {
+              vm.colorScheme(element.value);
+              vm.chartdata.labels.push(
+                moment(element.timestamp_measured).format("YYYY-MM-DD HH:mm")
+              );
+              vm.chartdata.datasets[0].backgroundColor.push(vm.color);
+              vm.chartdata.datasets[0].data.push(element.value);
+            }
+          }
+          vm.loaded = true;
+          vm.initalized = true;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
     overviewConcentration() {
-        const path = '/dashboard/' + `${this.location}` + '/airquality';
-        this.$router.push(path);
+      const path = "/dashboard/" + `${this.location}` + "/airquality";
+      this.$router.push(path);
     },
     colorScheme(colorValue) {
       this.color = "";
@@ -144,134 +313,38 @@ export default {
           this.color = "";
           break;
       }
-    },
-    onSucces(pos) {
-      console.log(pos);
-      this.$store.state.latitude = pos.coords.latitude;
-      this.$store.state.longitude = pos.coords.longitude;
-    },
-    onError(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    },
   },
+},
   computed: {
-     myStyles () {
+    myStyles() {
       return {
         height: `${this.height}px`,
-        position: 'relative'
-      }
+        position: "relative",
+      };
     },
     location() {
-        return this.$store.state.location;
+      return this.$store.state.location;
     },
     currentAirquality() {
-        return this.$store.state.currentAirquality;
+      return this.$store.state.currentAirquality;
     },
     currentDate() {
-        return this.$store.state.currentDate;
+      return this.$store.state.currentDate;
     },
     colorStroke() {
-        return this.$store.state.colorStroke;
+      return this.$store.state.colorStroke;
     },
     currentConcentration() {
-        return this.$store.state.currentConcentration;
-    }
-
+      return this.$store.state.currentConcentration;
+    },
   },
-  async created() {
+  created() {
     this.$store.dispatch("setDefaultAppbar");
     this.$store.state.currentDate = moment().format("MMM DD HH:mm");
-    var vm = this;
-    if (
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      )
-    ) {
-      Geolocation.getCurrentPosition()
-        .then(function (response) {
-          console.log(response);
-          vm.$store.state.longitude = response.coords.longitude;
-          vm.$store.state.latitude = response.coords.latitude;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    } else {
-      if (navigator.geolocation) {
-        await navigator.geolocation.getCurrentPosition(
-          this.onSucces,
-          this.onError
-        );
-      } else {
-        console.log("hallo mijn naam is Hans");
-      }
-    }
-    setTimeout(() => {
-      axios
-        .get("http://www.mapquestapi.com/geocoding/v1/reverse", {
-          params: {
-            key: "SGTAMDyeoX9iqBrAGn8KVEuKHAIvC2HK",
-            location: this.$store.state.latitude + "," + this.$store.state.longitude,
-          },
-        })
-        .then(function (response) {
-          console.log(response);
-          console.log(response.data.results);
-          for (let index = 0; index < response.data.results.length; index++) {
-            const element = response.data.results[index];
-            for (let index = 0; index < element.locations.length; index++) {
-              const location = element.locations[index];
-              console.log(location);
-              vm.$store.state.location = location.street;
-              vm.$store.state.fullLocation = location.street + ", " + location.adminArea5;
-              console.log(vm.$store.state.location);
-            }
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-
-      axios
-        .get("https://api.luchtmeetnet.nl/open_api/concentrations", {
-          params: {
-            formula: "LKI",
-            longitude: this.$store.state.longitude,
-            latitude: this.$store.state.latitude,
-          },
-        })
-        .then(function (response) {
-          console.log(response.data.data);
-          for (let index = 0; index < response.data.data.length; index++) {
-            const element = response.data.data[index];
-            const currentTime = moment().format("YYYY-MMM-DD HH:00");
-            if(moment(currentTime).isSame(moment(element.timestamp_measured).format("YYYY-MM-DD HH:mm"))) {
-                vm.colorScheme(element.value);
-                vm.$store.state.colorStroke = vm.color;
-                vm.$store.state.currentAirquality = vm.airQualityPerHour;
-                vm.$store.state.currentConcentration = Math.round(element.value);
-                vm.chartdata.labels.push(moment(element.timestamp_measured).format("YYYY-MM-DD HH:mm"));
-                vm.chartdata.datasets[0].backgroundColor.push(vm.color);
-                vm.chartdata.datasets[0].data.push(element.value);
-            }
-            else {
-                vm.colorScheme(element.value);
-                vm.chartdata.labels.push(moment(element.timestamp_measured).format("YYYY-MM-DD HH:mm"));
-                vm.chartdata.datasets[0].backgroundColor.push(vm.color);
-                vm.chartdata.datasets[0].data.push(element.value);
-            }
-          }
-          vm.loaded = true;
-          vm.initalized = true;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }, 5000);
+    this.initalizeComponent();
   },
-};
+}
 </script>
 
 <style>
-
 </style>
