904bf03f794caba817a088e3029d17da2ad29c43
NencioGizuh <44736674+NencioGizuh@users.noreply.github.com>
2020-12-05 19:30:05 +0100

    Update vuetify.js
    Foutje

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+      theme: {
+        themes: {
+            light: {
+                primary: "#0086d7",
+                secondary: "#cfecf7",
+                accent: "#ef7d00"
+            }
+        }
+    }
 });
