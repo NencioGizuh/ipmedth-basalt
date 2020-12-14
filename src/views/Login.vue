33ce6c73cb1d7560368562dbe1d26af7b94f7d50
Gaj Cakarevic <gaj.cakarevic@gmail.com>
2020-12-14 12:56:17 +0100

    Revert "login pagina"
    This reverts commit 78b95087913c5b9287b68dc0682eeaf1d4a81c76.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,10 @@
 <template>
-    <div class="centered">
-        <v-img class="logo"
-        src="@/assets/logos_en_blocks/logo-basalt.png"
-        width="234px"></v-img>
-        
-        <v-text-field
-              label="E-mail"
-              placeholder="e-mail"
-        ></v-text-field>
-        <v-text-field
-              label="Password"
-              placeholder="password"
-              type="password"
-        ></v-text-field>
-        <v-btn block color="accent" @click="login" class="login">
+    <div>
+        <v-btn block color="accent" @click="login">
             Inloggen
         </v-btn>
-        <p class="registry__button">
-            Nog geen account? klik <a to="/dashboard">hier</a> om je te registreren
-        </p>
+
+        Login
     </div>
 </template>
 
@@ -40,27 +26,6 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
-.registry__button {
-    font-size: 0.7em;
-    text-align: center;
-    margin: 10px 0 0 0;
-}
-
-.logo {
-    margin: 0 auto 60px auto;
-} 
-
-.centered {
-    position: relative;
-    top: 20%;
-}
-
-.login {
-    margin: 30px 0 0 0;
-}
-
-    
+<style lang="scss">
 
 </style>
