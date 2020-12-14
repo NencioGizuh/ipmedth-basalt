e01d12f2355639db88155133be8fd969db5e3593
Gaj Cakarevic <gaj.cakarevic@gmail.com>
2020-12-14 12:59:34 +0100

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,24 @@
 <template>
-    <div>
-        <v-btn block color="accent" @click="login">
+    <div class="centered">
+        <v-img class="logo"
+        src="@/assets/logos_en_blocks/logo-basalt.png"
+        width="234px"></v-img>
+        
+        <v-text-field
+              label="E-mail"
+              placeholder="e-mail"
+        ></v-text-field>
+        <v-text-field
+              label="Password"
+              placeholder="password"
+              type="password"
+        ></v-text-field>
+        <v-btn block color="accent" @click="login" class="login">
             Inloggen
         </v-btn>
-
-        Login
+        <p class="registry__button">
+            Nog geen account? klik <a to="/dashboard">hier</a> om je te registreren
+        </p>
     </div>
 </template>
 
@@ -26,6 +40,27 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+.registry__button {
+    font-size: 0.7em;
+    text-align: center;
+    margin: 10px 0 0 0;
+}
+
+.logo {
+    margin: 0 auto 60px auto;
+} 
+
+.centered {
+    position: relative;
+    top: 20%;
+}
+
+.login {
+    margin: 30px 0 0 0;
+}
+
+    
 
 </style>
