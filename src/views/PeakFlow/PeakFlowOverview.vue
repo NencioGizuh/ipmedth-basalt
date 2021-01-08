80f4d4001ca92080d59b9bbb007459045a5602d6
Lara van Diemen <laravandiemen23@gmail.com>
2021-01-08 10:37:32 +0100

    Add peakflow to dashboard and custom messages created  based on peak flow

--- src/views/PeakFlow/PeakFlowOverview.vue
+++ src/views/PeakFlow/PeakFlowOverview.vue
@@ -5,6 +5,43 @@
                 <v-icon left>mdi-plus</v-icon>
                 Nieuwe meting toevoegen
             </v-btn>
+            
+            <v-card class="mt-3" v-if="getPeakFlow.length > 0">
+                <v-card-text>
+                    <div v-if="getDaysAgo(getPeakFlow[0].date) > 2">
+                        Het is al {{getDaysAgo(getPeakFlow[0].date)}} dagen geleden dat je je laatste peak flow meting hebt ingevoerd. Door het doen van peak flow metingen kan je beter handelen naar je astma. Voer dus zo snel mogelijk weer een meting uit!
+                    </div>
+                    <div v-else>
+                        <div v-if="getZone(Math.max(getPeakFlow[0].measurement_one, getPeakFlow[0].measurement_two, getPeakFlow[0].measurement_three)) === 'groene'">
+                            Jouw laatste peak flow meting van {{getPeakFlow[0].date | formatDate}} bevindt zich in de <b>{{getZone(Math.max(getPeakFlow[0].measurement_one, getPeakFlow[0].measurement_two, getPeakFlow[0].measurement_three))}} zone</b>. Blijf je medicatie nemen zoals voorgeschreven om dit vol te blijven houden!
+                        </div>
+                        <div v-if="getZone(Math.max(getPeakFlow[0].measurement_one, getPeakFlow[0].measurement_two, getPeakFlow[0].measurement_three)) === 'gele'">
+                            Jouw laatste peak flow meting van {{getPeakFlow[0].date | formatDate}} bevindt zich in de <b>{{getZone(Math.max(getPeakFlow[0].measurement_one, getPeakFlow[0].measurement_two, getPeakFlow[0].measurement_three))}} zone</b>. Ga naar het <router-link class="actionplan-link" to="/actionplan">actieplan</router-link> om te kijken hoe je je klachten kan verbeteren. Mochten de klachten langer dan 48 uur aanhouden, ga dan naar de oranje zone!
+                        </div>
+                        <div v-if="getZone(Math.max(getPeakFlow[0].measurement_one, getPeakFlow[0].measurement_two, getPeakFlow[0].measurement_three)) === 'rode'">
+                            <p>
+                                Jouw laatste peak flow meting van {{getPeakFlow[0].date | formatDate}} bevindt zich in de <b>{{getZone(Math.max(getPeakFlow[0].measurement_one, getPeakFlow[0].measurement_two, getPeakFlow[0].measurement_three))}} zone</b>! Neem contact op met jouw long- of huisarts! Kijk verder naar het <router-link class="actionplan-link" to="/actionplan">actieplan</router-link> om te zien welke handelingen je moet ondernemen.
+                            </p>
+                            <v-row>
+                                <v-col v-if="getPhoneNumberGP">
+                                    <v-btn 
+                                        block
+                                        color="accent" 
+                                        :href="`tel:${getPhoneNumberGP}`"
+                                    >Bel Huisarts</v-btn>
+                                </v-col>
+                                <v-col v-if="getPhoneNumberLungSpecialist">
+                                    <v-btn 
+                                        block 
+                                        color="accent" 
+                                        :href="`tel:${getPhoneNumberLungSpecialist}`"
+                                    >Bel Longarts</v-btn>
+                                </v-col>
+                            </v-row>
+                        </div>
+                    </div>     
+                </v-card-text>
+            </v-card>
 
             <v-card class="mt-3">
                 <v-card-title>Laatste metingen</v-card-title>
@@ -72,15 +109,36 @@ export default {
         loadingPeakFlow() {
             return this.$store.getters.loadingPeakFlow;
         },
+        getPhoneNumberGP() {
+            return this.$store.getters.getPhoneNumberGP;
+        },
+        getPhoneNumberLungSpecialist() {
+            return this.$store.getters.getPhoneNumberLungSpecialist;
+        },
     },
     methods: {
         goToMeasurement(peakflowId) {
             this.$router.push(`/peakflow/${peakflowId}`)
+        },
+        getDaysAgo(date) {
+            const today = new Date();
+            return today.getDay() - new Date(date).getDay();
+        },
+        getZone(value) {
+            if (value > this.$store.getters.getPeakFlowZoneGreen) {
+                return "groene";
+            } else if (value > this.$store.getters.getPeakFlowZoneYellow) {
+                return "gele";
+            } else {
+                return "rode";
+            }
         }
     }
 }
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.actionplan-link {
+    color: $accent-color !important;
+}
 </style>
